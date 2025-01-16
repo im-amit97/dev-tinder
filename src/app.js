9622d96bdfd37d3d2a993d43c508a807612c38b2
Amit <sarodeamit111@gmail.com>
2025-01-16 19:57:01 +0530

    created http apis for testing

--- src/app.js
+++ src/app.js
@@ -2,14 +2,38 @@ const express = require('express');
 
 const app = express(); //instance of express
 
-app.use("/hello", (req, res) => {
-    res.send('This is Hello Api')
-})
-
-app.use('/', (req, res) => {
-    res.send('Hello Node JS');
+app.get('/user', (req, res) => {
+    res.send({
+        'firstname': 'John',
+        'lastname': 'Sena'
+    })
 });
 
-app.listen(7777, () => {
-    console.log('Node server started on port 7777');
+app.patch('/user', (req, res) => {
+    res.send({
+        responseCode: 200,
+        response: 'User updated Successfully'
+    })
+});
+
+app.post('/user', (req, res) => {
+    res.send({
+        responseCode: 200,
+        response: 'User Created Successfully'
+    })
+});
+
+app.delete('user', (req, res) => {
+    res.send({
+        responseCode: 200,
+        response: 'User Deleted Successfully'
+    })
+})
+
+app.use("/hello", (req, res) => {
+    res.send('This is Hello Api')
+});
+
+app.listen(8000, () => {
+    console.log('Node server started on port 8000');
 })
