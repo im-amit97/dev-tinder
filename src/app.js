9fa03f2d86447be1c830e7dbf58d6148000afd93
Amit <sarodeamit111@gmail.com>
2025-01-17 22:05:30 +0530

    created delete and update user api

--- src/app.js
+++ src/app.js
@@ -17,33 +17,58 @@ app.post("/signin", async (req, res) => {
   }
 });
 
-app.get("/user", async(req, res) => {
-    try {
-        const userEmailId = req.body.emailId;
-        const user = await User.findOne({email: userEmailId });
-        if (!user) {
-            res.status(404).send('User Not Found');
-        } else {
-            res.send(user);
-        }
-    } catch(err) {
-        res.status(500).send('Internal Server Error');
+app.get("/user", async (req, res) => {
+  try {
+    const userEmailId = req.body.emailId;
+    const user = await User.findOne({ email: userEmailId });
+    if (!user) {
+      res.status(404).send("User Not Found");
+    } else {
+      res.send(user);
     }
-
+  } catch (err) {
+    res.status(500).send("Internal Server Error");
+  }
 });
 
-app.get("/feed", async(req, res) => {
-    try {
-        const users = await User.find({});
-        if (users.length === 0) {
-            res.status(404).send('Users Not Found');
-        } else {
-            res.send(users);
-        }
-    } catch(err) {
-        res.status(500).send('Internal Server Error');
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      res.status(404).send("Users Not Found");
+    } else {
+      res.send(users);
     }
+  } catch (err) {
+    res.status(500).send("Internal Server Error");
+  }
+});
 
+app.delete("/user", async (req, res) => {
+  try {
+    const userId = req?.body?.userId;
+    const user = await User.findByIdAndDelete(userId);
+    if (!user) {
+      res.status(404).send("No User Found");
+    } else {
+      res.send("User Deleted Successfully");
+    }
+  } catch (err) {
+    res.status(500).send("Internal Server Error " + err);
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req?.body?.userId, req?.body);
+    if (!user) {
+      res.status(404).send("No User Found");
+    } else {
+      res.send("User Updated Successfully");
+    }
+  } catch (err) {
+    res.status(500).send("Something Went Wrong " + err);
+  }
 });
 
 connectDB().then(() => {
